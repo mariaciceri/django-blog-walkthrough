4f674d0c24ea3d0a35317ebf4d6ab198f635143a
Maria <mariafacchin00@hotmail.com>
2024-12-26 13:30:17 +0100

    Display posts to user

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views import generic
+from .models import Post
 
-def my_blog(request):
-    return HttpResponse("Hello, world. You're at the blog index.")
+class PostList(generic.ListView): #listview is a class-based view that lists objects
+    queryset = Post.objects.filter(status=1) #queryset is a variable that contains all the objects of the Post model
+    template_name = "post_list.html"
