4e2bb22caee2f988593103333069e40ad3ad78ad
Maria <mariafacchin00@hotmail.com>
2024-12-27 10:26:22 +0100

    Add html and css

--- blog/views.py
+++ blog/views.py
@@ -4,4 +4,5 @@ from .models import Post
 
 class PostList(generic.ListView): #listview is a class-based view that lists objects
     queryset = Post.objects.filter(status=1) #queryset is a variable that contains all the objects of the Post model
-    template_name = "post_list.html"
+    template_name = "blog/index.html"
+    paginate_by = 6
