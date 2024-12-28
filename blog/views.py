611619d166303d6cd5ecb449858860b1960f5206
Maria <mariafacchin00@hotmail.com>
2024-12-28 13:58:29 +0100

    style post detail page

--- blog/views.py
+++ blog/views.py
@@ -3,29 +3,29 @@ from django.views import generic
 from .models import Post
 
 class PostList(generic.ListView): #listview is a class-based view that lists objects
+    queryset = Post.objects.filter(status=1)
+    template_name = "blog/index.html"
+    paginate_by = 6
 
-    def post_detail(request, slug):
+def post_detail(request, slug):
         """
         Display an individual :model:`blog.Post`.
 
         **Context**
 
         ``post``
-        An instance of :model:`blog.Post`.
+            An instance of :model:`blog.Post`.
 
         **Template:**
 
         :template:`blog/post_detail.html`
         """
-        
-        queryset = Post.objects.filter(status=1) #queryset is a variable that contains all the objects of the Post model
+
+        queryset = Post.objects.filter(status=1)
         post = get_object_or_404(queryset, slug=slug)
 
         return render(
             request,
-            'blog/post_detail.html',
-            {'post': post},
+            "blog/post_detail.html",
+            {"post": post},
         )
-    
-    template_name = "blog/index.html"
-    paginate_by = 6
